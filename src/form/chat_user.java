7ed1443fba95af5ac1a88b678f7928f2d5bd9d9a
user <callmesammy@yahoo.com>
2022-07-04 11:26:58 +0100

    My Git commit

---
+++ src/form/chat_user.java
@@ -0,0 +1,57 @@
+
+package form;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author user
+ */
+public class chat_user extends javax.swing.JLayeredPane {
+
+    
+    public chat_user() {
+        initComponents();  
+        txt.setBackground(new Color(242,242,242));
+    }
+    
+    public void username(String name){
+        username(name);
+    }
+    public void setText(String text){
+        txt.setText(text);
+        txt.setTime("01:30pm");
+        txt.setsuccess();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        txt = new form.Chat_item();
+
+        setLayer(txt, javax.swing.JLayeredPane.DEFAULT_LAYER);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(0, 0, 0)
+                .addComponent(txt, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(0, 0, 0))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(0, 0, 0)
+                .addComponent(txt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private form.Chat_item txt;
+    // End of variables declaration//GEN-END:variables
+}
