6c314a226f40adf3fe6dce00ff2275f386afb67d
user <callmesammy@yahoo.com>
2022-07-04 22:21:48 +0100

    My Git commit2

--- src/form/Chat_body.java
+++ src/form/Chat_body.java
@@ -17,63 +17,43 @@ public class Chat_body extends javax.swing.JPanel {
         init();
         addTextLeft(" 23/05/2016 · ‎Notes is the best place to jot down quick thoughts "
                 + "or to save longer notes filled with checklists, images, web links, "
-                + "scanned documents, handwritten notes, or sketches. And with");
+                + "scanned documents, handwritten notes, or sketches. And with", "Jokn");
         addTextRight("Is your security info still accurate?\n" +
 "We just want to make sure this security info is up-to-date, so we can use it to verify your identity "
                 + "or alert you if there's ever a problem with your account.");
+        addDate("09/04/2022");
         
         
           addTextLeft(" 23/05/2016 · ‎Notes is the best place to jot down quick thoughts "
                 + "or to save longer notes filled with checklists, images, web links, "
-                + "scanned documents, handwritten notes, or sketches. And with");
-        addTextRight("Is your security info still accurate?\n" +
-"We just want to make sure this security info is up-to-date, so we can use it to verify your identity "
-                + "or alert you if there's ever a problem with your account.");  addTextLeft(" 23/05/2016 · ‎Notes is the best place to jot down quick thoughts "
+                + "scanned documents, handwritten notes, or sketches. And with", "Sam");
+              addTextLeft(" 23/05/2016 · ‎Notes is the best place to jot down quick thoughts "
                 + "or to save longer notes filled with checklists, images, web links, "
-                + "scanned documents, handwritten notes, or sketches. And with");
-        addTextRight("Is your security info still accurate?\n" +
-"We just want to make sure this security info is up-to-date, so we can use it to verify your identity "
-                + "or alert you if there's ever a problem with your account.");  addTextLeft(" 23/05/2016 · ‎Notes is the best place to jot down quick thoughts "
-                + "or to save longer notes filled with checklists, images, web links, "
-                + "scanned documents, handwritten notes, or sketches. And with");
-        addTextRight("Is your security info still accurate?\n" +
-"We just want to make sure this security info is up-to-date, so we can use it to verify your identity "
-                + "or alert you if there's ever a problem with your account.");  addTextLeft(" 23/05/2016 · ‎Notes is the best place to jot down quick thoughts "
-                + "or to save longer notes filled with checklists, images, web links, "
-                + "scanned documents, handwritten notes, or sketches. And with");
-        addTextRight("Is your security info still accurate?\n" +
-"We just want to make sure this security info is up-to-date, so we can use it to verify your identity "
-                + "or alert you if there's ever a problem with your account.");  addTextLeft(" 23/05/2016 · ‎Notes is the best place to jot down quick thoughts "
-                + "or to save longer notes filled with checklists, images, web links, "
-                + "scanned documents, handwritten notes, or sketches. And with");
-        addTextRight("Is your security info still accurate?\n" +
-"We just want to make sure this security info is up-to-date, so we can use it to verify your identity "
-                + "or alert you if there's ever a problem with your account.");  addTextLeft(" 23/05/2016 · ‎Notes is the best place to jot down quick thoughts "
-                + "or to save longer notes filled with checklists, images, web links, "
-                + "scanned documents, handwritten notes, or sketches. And with");
-        addTextRight("Is your security info still accurate?\n" +
-"We just want to make sure this security info is up-to-date, so we can use it to verify your identity "
-                + "or alert you if there's ever a problem with your account.");  addTextLeft(" 23/05/2016 · ‎Notes is the best place to jot down quick thoughts "
-                + "or to save longer notes filled with checklists, images, web links, "
-                + "scanned documents, handwritten notes, or sketches. And with");
-        addTextRight("Is your security info still accurate?\n" +
-"We just want to make sure this security info is up-to-date, so we can use it to verify your identity "
-                + "or alert you if there's ever a problem with your account.");  addTextLeft(" 23/05/2016 · ‎Notes is the best place to jot down quick thoughts "
-                + "or to save longer notes filled with checklists, images, web links, "
-                + "scanned documents, handwritten notes, or sketches. And with");
-        addTextRight("Is your security info still accurate?\n" +
-"We just want to make sure this security info is up-to-date, so we can use it to verify your identity "
-                + "or alert you if there's ever a problem with your account.");  addTextLeft(" 23/05/2016 · ‎Notes is the best place to jot down quick thoughts "
-                + "or to save longer notes filled with checklists, images, web links, "
-                + "scanned documents, handwritten notes, or sketches. And with");
-        addTextRight("Is your security info still accurate?\n" +
-"We just want to make sure this security info is up-to-date, so we can use it to verify your identity "
-                + "or alert you if there's ever a problem with your account.");  addTextLeft(" 23/05/2016 · ‎Notes is the best place to jot down quick thoughts "
-                + "or to save longer notes filled with checklists, images, web links, "
-                + "scanned documents, handwritten notes, or sketches. And with");
+                + "scanned documents, handwritten notes, or sketches. And with", "Jojo");
         addTextRight("Is your security info still accurate?\n" +
 "We just want to make sure this security info is up-to-date, so we can use it to verify your identity "
                 + "or alert you if there's ever a problem with your account.");
+        
+             addDate("09/04/2022");
+        
+          addTextLeft(" 23/05/2016 · ‎Notes is the best place to jot down quick thoughts "
+                + "or to save longer notes filled with checklists, images, web links, "
+                + "scanned documents, handwritten notes, or sketches. And with", "Sam");
+               addDate("09/04/2022");
+        
+        addTextLeft(" 23/05/2016 · ‎Notes is the best place to jot down quick thoughts "
+                + "or to save longer notes filled with checklists, images, web links, "
+                + "scanned documents, handwritten notes, or sketches. And with", "Jonny");
+        addTextRight("Is your security info still accurate?\n" +
+"We just want to make sure this security info is up-to-date, so we can use it to verify your identity "
+                + "or alert you if there's ever a problem with your account.");
+        
+             addDate("09/04/2022");
+        
+          addTextLeft(" 23/05/2016 · ‎Notes is the best place to jot down quick thoughts "
+                + "or to save longer notes filled with checklists, images, web links, "
+                + "scanned documents, handwritten notes, or sketches. And with", "MOOn");
+    
     }
 
     private void init(){
@@ -81,7 +61,22 @@ public class Chat_body extends javax.swing.JPanel {
         sp.setVerticalScrollBar(new ScrollBar());
         sp.getVerticalScrollBar().setBackground(Color.WHITE);
     }
-    
+      private void addTextLeft(String text, String name){
+        chat_user item = new chat_user();
+        item.setText(text);
+        body.add(item, "wrap, w 100::75%");
+        item.usernameProfile(name);
+        body.repaint();
+        body.revalidate();
+    }
+      
+      private void addDate(String date){
+          Chat_date item = new Chat_date();
+          item.setDate(date);
+          body.add(item, "wrap, al center");
+          body.repaint();
+          body.revalidate();
+      }
     private void addTextLeft(String text){
         chat_left item = new chat_left();
         item.setText(text);
@@ -92,7 +87,7 @@ public class Chat_body extends javax.swing.JPanel {
         private void addTextRight(String text){
         Chat_right itm = new Chat_right();
         itm.setText(text);
-        body.add(itm, "wrap, al right, w ::75%");
+        body.add(itm, "wrap, al right, w 100::75%");
         body.repaint();
         body.revalidate();
     }
